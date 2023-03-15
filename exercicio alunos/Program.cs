35b6eae159ecba8d349b911f5c263b7dc5b11f4e
Gabriel <100374129+projetosprimogabriel@users.noreply.github.com>
2023-03-15 15:37:03 -0300

    Add files via upload

---
+++ exercicio alunos/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace exercicio_alunos;
+class Program
+{
+    static void Main(string[] args)
+    {
+       alunos aluno1=  new alunos();
+       aluno1.nome = "outro aluno";
+       aluno1.nota1 = 4;
+       aluno1.nota2 = 5;
+
+       aluno1.mensagem();
+    }
+}
